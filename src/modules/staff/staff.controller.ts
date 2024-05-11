d5735617f57cefa4549f0da38060e0ca2bb588bf
barsbold-b <barsbold.coding@gmail.com>
2024-05-11 22:05:09 +0800

    feat: 🎸 added staff controller

---
+++ src/modules/staff/staff.controller.ts
@@ -0,0 +1,25 @@
+import { Controller, Get, Options, Query } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { StaffService } from './staff.service';
+import { QueryDto } from 'globals/dto/query.dto';
+
+@ApiTags('staffs')
+@Controller('staffs')
+export class StaffController {
+  constructor(private readonly staffService: StaffService) {}
+
+  @Get('about')
+  async getFind() {
+    return '';
+  }
+
+  @Get()
+  async findAll(@Query() query: QueryDto) {
+    return await this.staffService.findAll(query);
+  }
+
+  @Options()
+  async findOptions() {
+    return await this.staffService.findOptions();
+  }
+}
--- src/modules/staff/staff.service.ts
+++ src/modules/staff/staff.service.ts
@@ -1,12 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
+import { QueryDto } from 'globals/dto/query.dto';
 import { Staff } from 'models/staff.entity';
 
 @Injectable()
 export class StaffService {
   constructor(@InjectModel(Staff) private staffRepository: typeof Staff) {}
 
-  async findAll() {
+  async findAll(query: QueryDto) {
     return await this.staffRepository.findAll();
+  }
+
+  async findOptions() {
+    return this.staffRepository.getAttributes();
   }
 }
